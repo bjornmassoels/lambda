dd32569249e8e8ac104c5e0e7bb258cd5306f36a
bjorn <bjornlain2@outlook.com>
2023-10-15 13:28:54 +0200

    FINALLY WORKING

--- index.mjs
+++ index.mjs
@@ -1,18 +1,17 @@
 // importeer de specifieke AWS service pakketten die je nodig hebt
 import { S3Client, GetObjectCommand, ListObjectsCommand, PutObjectCommand, UploadPartCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import archiver from 'archiver';
-import streamToPromise from 'stream-to-promise';
 import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
 import { createRequest } from '@aws-sdk/util-create-request';
 import { formatUrl } from '@aws-sdk/util-format-url';
+import fs from 'fs';
+import toArray from 'stream-to-array';
 
 // maak en gebruik v3 service clients, vervang globale waarden zoals region door configuratiewaarden die als argumenten worden doorgegeven aan de client
 const s3Client = new S3Client({ region: "eu-north-1" , endpoint: "https://s3.eu-north-1.amazonaws.com" });
 export const handler = async (event, context) => {
     try  {
         let archive = archiver('zip', {
-            gzip:true,
             zlib: { level: 9 }
         });
         const zipFileName = event.zipFileName;
@@ -23,6 +22,7 @@ export const handler = async (event, context) => {
 
         const inputBucket = 'selux-riolering-pdf-saver';
 
+        let output = fs.createWriteStream('/tmp/' + zipFileName);
 
         archive.on('error', function(err) {
             console.log('Archive error:', err);
@@ -39,115 +39,87 @@ export const handler = async (event, context) => {
                 throw err;
             }
         });
-        let endedStreams = 0;
-        const listObjectsInput = {
-            Bucket: inputBucket,
-            Prefix: groupName + "/"
-        };
-        console.log(zipFileName);
         console.log(groupName)
 
-        let { Contents } = await s3Client.send(new ListObjectsCommand(listObjectsInput));
-        console.log('Contents:', Contents);
-        if (!Contents) {
-            new Error("Contents is not defined.");
-        }
+        archive.pipe(output);
 
+        await processPDFs(event.projectIds, event.groupName, inputBucket, archive, s3Client);
 
+        // Process slokkerProjectIds PDFs, 2 at a time
+        await processPDFs(event.slokkerProjectIds, event.groupName, inputBucket, archive, s3Client);
 
-        for(let file of projectIds){
-            file = file + '.pdf';
-            const inputParams = {
-                Bucket: inputBucket,
-                Key: groupName + '/' + file
-            };
-            let inputData;
-
-            try {
-                console.log(file)
-                try {
-                    inputData = await s3Client.send(new GetObjectCommand(inputParams));
-                } catch (err) {
-                    console.log("Error fetching PDF:", err);
-                    throw err;
-                }
-                archive.append(inputData.Body, { name: file});
-            } catch (err) {
-                console.log(`Error getting object ${file} from bucket:`, err);
-                continue;
-            }
-        }
-
-
-        for(let file of slokkerProjectIds){
-            file = file + '.pdf';
-            const inputParams = {
-                Bucket: inputBucket,
-                Key: groupName + '/' + file
-            };
-            let inputData;
-
-            try {
-                console.log(file)
-                try {
-                    inputData = await s3Client.send(new GetObjectCommand(inputParams));
-                } catch (err) {
-                    console.log("Error fetching PDF:", err);
-                    throw err;
-                }
-                archive.append(inputData.Body, { name: file});
-            } catch (err) {
-                console.log(`Error getting object ${file} from bucket:`, err);
-                continue;
-            }
-        }
-
-        console.log('before close init');
-
-        const endArchive = new Promise(async (resolve, reject) => {
-                console.log('Archive ended');
-                try {
-                    const outputParams = {
-                        Bucket: "pdfzipbucket",
-                        Key: zipFileName,
-                        Body: await streamToPromise(archive),
-                        ContentType: 'application/zip'
-                    };
-
-                    s3Client.send(new PutObjectCommand(outputParams)).then(async (data) => {
-                        console.log('after send');
-                        const signer = new S3RequestPresigner(s3Client.config);
-                        const request = await createRequest(s3Client, new GetObjectCommand({
-                            Bucket: outputParams.Bucket,
-                            Key: outputParams.Key
-                        }));
-                        const signedUrl = formatUrl(await signer.presign(request));
-                        console.log('signedUrl:', signedUrl);
-                        resolve({ statusCode: 200, url: signedUrl });
-                    });
-                } catch (error) {
-                    // error handling.
-                    console.log(error);
-                    reject(error);
-                } finally {
-                    // finally.
-                    console.log("finalize");
-                }
-        });
         console.log('before finalize');
-        await archive.finalize();
-        console.log('after finalize');
-        // Now you can await this promise.
-        try {
-            const result = await endArchive;
-            console.log(result);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
+        return await new Promise((resolve, reject) => {
+            output.on('finish', async () => {
+                console.log(archive.pointer() + ' total bytes');
+                // Now upload the file to S3
+                const filePath = '/tmp/' + zipFileName;
+                const fileStream = fs.createReadStream(filePath);
+                const outputParams = {
+                    Bucket: "pdfzipbucket",
+                    Key: zipFileName,
+                    Body: fileStream,
+                    ContentType: 'application/zip'
+                };
+                console.log('before send2')
+                try {
+                    await s3Client.send(new PutObjectCommand(outputParams));
+                    console.log('after send');
+                    const signer = new S3RequestPresigner(s3Client.config);
+                    const request = await createRequest(s3Client, new GetObjectCommand({
+                        Bucket: outputParams.Bucket,
+                        Key: outputParams.Key
+                    }));
+                    const signedUrl = formatUrl(await signer.presign(request));
+                    console.log('signedUrl:', signedUrl);
+                    resolve({ statusCode: 200, url: signedUrl });
+                } catch (ex) {
+                    reject(ex);
+                }
+            });
+
+            archive.finalize().then(() => {
+                console.log('finalize done');
+            }).catch(reject);  // Catch any errors that might occur during archive.finalize()
+        });
 
     } catch (ex) {
         console.log(ex);
         return { statusCode: 500 };
     }
 };
+
+
+async function processPDFs(projectIds, groupName, inputBucket, archive, s3Client) {
+    for (let i = 0; i < projectIds.length; i++) {
+        const file = projectIds[i] + '.pdf';
+        const inputParams = {
+            Bucket: inputBucket,
+            Key: groupName + '/' + file,
+        };
+        try {
+            console.log(`Fetching ${file}`);
+            const inputData = await s3Client.send(new GetObjectCommand(inputParams));
+
+            // Convert the stream to a Buffer
+            const arr = await toArray(inputData.Body);
+            const buffer = Buffer.concat(arr);
+
+            // Write the Buffer to a local file
+            fs.writeFileSync('/tmp/' + file, buffer);
+
+            console.log(`Successfully fetched ${file}`);
+
+            archive.append(fs.createReadStream('/tmp/' + file), { name: file});
+            await sleep(100)
+        } catch (err) {
+            console.log(`Error getting object ${file} from bucket:`, err);
+        }
+
+        // Sleep for 1 second (1000 milliseconds) before next iteration
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
